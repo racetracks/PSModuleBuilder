9db9c320d07bdd5a8572891dbcea3409c805b24c
racetracks <95535303+racetracks@users.noreply.github.com>
2024-06-19 02:19:24 +0930

    Update CreateModule.ps1

--- build/CreateModule.ps1
+++ build/CreateModule.ps1
@@ -9,20 +9,20 @@ $ModuleFunctionsToExport = @('Get-Hello','Get-Goodbye')
 
 
 
-mkdir "..\$ModuleName"
-mkdir ..\$ModuleName\Functions
+mkdir "..\Modules\$ModuleName"
+mkdir ..\Modules\$ModuleName\Functions
 $psmcontents = '# Import all function files from the Functions directory
 Get-ChildItem -Path "$PSScriptRoot\Functions" -Filter *.ps1 | ForEach-Object {
     . $_.FullName
 }'
 
 
-New-Item "..\$ModuleName\$ModuleName.psm1" -ItemType File
-$psmcontents | Out-File -FilePath "..\$ModuleName\$ModuleName.psm1"
+New-Item "..\Modules\$ModuleName\$ModuleName.psm1" -ItemType File
+$psmcontents | Out-File -FilePath "..\Modules\$ModuleName\$ModuleName.psm1"
 
 
 $Params = @{ 
-    "Path" 				= "..\$($modulename)\$($modulename).psd1"
+    "Path" 				= "..\Modules\$($modulename)\$($modulename).psd1"
     "Author" 			= $Author 
     "CompanyName" 			= '$Company
     "RootModule" 			= "$($modulename).psm1"
